4404860f7cb8fe1c3ba31bea9a2865caa0b3d9b4
mollaev <mollaev@tk34.mskobr.ru>
2022-05-19 14:17:03 +0300

    Добавьте файлы проекта.

---
+++ Delegate/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringHelper helper = new StringHelper();
+
+            CountDelegate d1 = helper.GetCount;
+            CountDelegate d2 = helper.GetCountSymbolA;
+
+            string testString = "LAMP";
+
+            Console.WriteLine("Общее количество символов: {0}", TestDelegate(d1, testString));
+            Console.WriteLine("Количество символов A: {0}", TestDelegate(d2, testString));
+
+            Console.ReadLine();
+        }
+
+        static int TestDelegate(CountDelegate method, string testString)
+        {
+            return method(testString);
+        }
+    }
+}
---
+++ Delegate/StringHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+    public delegate int CountDelegate(string message);
+    class StringHelper
+    {
+        public int GetCount(string inputString)
+        {
+            return inputString.Length;
+        }
+        public int GetCountSymbolA(string inputString)
+        {
+            return inputString.Count(c => c == 'A');
+        }
+        public int GetCountSymbol(string inputString, char symbol)
+        {
+            return inputString.Count(c => c == symbol);
+        }
+
+
+    }
+}
